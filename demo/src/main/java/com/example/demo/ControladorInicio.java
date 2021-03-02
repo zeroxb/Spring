c7f131d95f36cf41f79eec3ebda23db353953b5e
JOSE ARTURO PEREZ ENRIQUEZ <japereze@bancoazteca.com>
2021-03-02 17:24:25 -0600

    se agrego el logger

--- demo/src/main/java/com/example/demo/ControladorInicio.java
+++ demo/src/main/java/com/example/demo/ControladorInicio.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
-
 import com.example.demo.domain.Persona;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +24,26 @@ public class ControladorInicio {
 
         String mensaje = "Hola Mundo con thymeleaf";
         Persona persona = new Persona();
+       
         persona.setNombre("Juan");
         persona.setApellido("Patiño");
         persona.setEmail("juanpatiño@gmail.com");
         persona.setTelefono("5546344578");
 
+        Persona persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("kgomez@gmail.com");
+        persona2.setTelefono("5598344238");
+
+        List<Persona> personas = Arrays.asList(persona,persona2);
+
+
         log.info("ejecutando el controlador SpringMVC");//Log de ejecución de Inicio
         model.addAttribute("mensaje", mensaje);
-        model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        //model.addAttribute("saludo", saludo);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
 
        return "index";       
     }
