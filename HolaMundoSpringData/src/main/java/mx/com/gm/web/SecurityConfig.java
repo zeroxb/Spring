d32d2ba4ac5f21d2f4135a46c5f4dad840ced163
Jose Arturo <josearturo@EKT1673013MACLAP.local>
2021-03-07 06:19:23 -0600

    Ya volvio a jalar el error estaba en el pom

--- HolaMundoSpringData/src/main/java/mx/com/gm/web/SecurityConfig.java
+++ HolaMundoSpringData/src/main/java/mx/com/gm/web/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+   protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication().withUser("admin")
         .password("{noop}RootmasterAdmin")
         .roles("ADMIN","USER")
