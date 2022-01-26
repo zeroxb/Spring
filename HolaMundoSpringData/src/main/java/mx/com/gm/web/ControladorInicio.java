14280062919759925f328d6e8899e99314dc5409
zero <zeroxb7500@gmail.com>
2022-01-26 12:20:09 -0600

    se agregan los datos
    se agregan datos

--- HolaMundoSpringData/src/main/java/mx/com/gm/web/ControladorInicio.java
+++ HolaMundoSpringData/src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -1,6 +1,5 @@
 package mx.com.gm.web;
 
-
 import lombok.extern.slf4j.Slf4j;
 import mx.com.gm.domain.Persona;
 import mx.com.gm.servicio.PersonaService;
@@ -18,58 +17,64 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
 @Controller
 @Slf4j
 public class ControladorInicio {
-    
+
     @Autowired
     private PersonaService personaService;
 
     private static final Logger log = LoggerFactory.getLogger(ControladorInicio.class);
-    
+
     @GetMapping("/")
-    public String inicio(Model model, @AuthenticationPrincipal User user){
+    public String inicio(Model model, @AuthenticationPrincipal User user) {
         var personas = personaService.listarPersonas();
-        log.info("Peresonas : "+ personas);
+        log.info("Personas : " + personas);
         log.info("ejecutando el controlador Spring MVC");
-        log.info("Nombre:"+ user);
+        log.info("Nombre:" + user);
         model.addAttribute("personas", personas);
+        var saldoTotal = 0D;
+
+        for (var p : personas) {
+            saldoTotal += p.getSaldo();
+        }
+
+        model.addAttribute("saldoTotal", saldoTotal);
+        model.addAttribute("totalClientes", personas.size());
+
         return "index";
     }
 
     @GetMapping("/agregar")
-    public String agregar(Persona persona) 
-    {
-        return"modificar";
-        
+    public String agregar(Persona persona) {
+        return "modificar";
+
     }
 
-    @PostMapping(value="/guardar")
+    @PostMapping(value = "/guardar")
     public String guardar(@Valid Persona persona, Errors errores) {
-        
-        if(errores.hasErrors()){
+
+        if (errores.hasErrors()) {
             return "modificar";
         }
-        
+
         personaService.guardar(persona);
         return "redirect:/";
     }
-    
-    @GetMapping("/editar/{idPersona}")
-    public String editar(Persona persona, Model model){
 
-        persona = personaService.encontrarPersona(persona); 
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model) {
+
+        persona = personaService.encontrarPersona(persona);
         model.addAttribute("persona", persona);
         return "modificar";
     }
 
     @GetMapping("/eliminar")
-    public String eliminar(Persona persona){
-        
+    public String eliminar(Persona persona) {
+
         personaService.eliminar(persona);
         return "redirect:/";
     }
-    
+
 }
